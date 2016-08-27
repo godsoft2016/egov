c2c881fb5a3e4837c3b351eaabf80893eae4c8fa
godsoft <dlqorgod@naver.com>
2016-08-27 15:30:04 +0900

    junit 테스트

---
+++ godsoft.com320/src/test/java/godsoft/com320/GodsoftCom320Test.java
@@ -0,0 +1,14 @@
+package godsoft.com320;
+
+import org.junit.Test;
+
+public class GodsoftCom320Test {
+
+	@Test
+	public void test() {
+		// fail("Not yet implemented");
+
+		System.out.println("이백행");
+	}
+
+}
