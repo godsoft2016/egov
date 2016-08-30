22744beb7df90e31b7b6df66478060d12053150c
이백행 <dlqorgod@naver.com>
2016-08-30 13:18:46 +0900

    test

---
+++ godsoft.com351/src/test/java/godsoft/com/GodsoftCom351Test.java
@@ -0,0 +1,14 @@
+package godsoft.com;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GodsoftCom351Test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
